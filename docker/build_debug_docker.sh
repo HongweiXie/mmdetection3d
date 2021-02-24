1f1115c8336edbb6815e2a217d6f6acb9b81800e
hongwei.xhw <hongwei.xhw@alibaba-inc.com>
2021-02-24 19:08:28 +0800

    add debug docker

---
+++ docker/build_debug_docker.sh
@@ -0,0 +1,3 @@
+
+docker build . -f docker/Dockerfile -t reg.docker.alibaba-inc.com/had-perc/op-shuofan_test:mmdet3d_debug
+docker push reg.docker.alibaba-inc.com/had-perc/op-shuofan_test:mmdet3d_debug
